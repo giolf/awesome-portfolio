c61e082c39a7a7a2f03fb100d35299b7159292dc
giolf <giovanni.far@gmail.com>
2016-11-05 18:43:51 +0100

    Gulp
    webpack implementation

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,34 @@
 var gulp = require('gulp');
+var webpack = require('webpack-stream');
 
 var paths = {
     apTmpl: {
-        src: [
-            'templates/single-project.php',
-            'templates/template-projects.php'
-        ],
-        dist: '../../themes/organic_photographer/'
+        src: 'templates/*   ',
+        dest: '../../themes/organic_photographer/'
     },
-    watch: {
-        src: ['templates/*']
+    js: {
+        entries: './src/js/entries/*'
+    },
+    webpack: {
+        config: './webpack.config.js',
+        dest: 'dist/js/'
     }
 };
 
 gulp.task('copy-ap-templates', function() {
     gulp.src(paths.apTmpl.src)
-    .pipe(gulp.dest(paths.apTmpl.dist));
+    .pipe(gulp.dest(paths.apTmpl.dest));
+});
+
+
+gulp.task('webpack', function() {
+    return webpack(require(paths.webpack.config))
+        .pipe(gulp.dest(paths.webpack.dest));
 });
 
 gulp.task('watch', function() {
- gulp.watch(paths.watch.src, ['copy-ap-templates']);
+    gulp.watch(paths.apTmpl.src, ['copy-ap-templates']);
+    gulp.watch(paths.js.entries, ['webpack']);
 });
 
-gulp.task('default', ['copy-ap-templates']);
+gulp.task('default', ['copy-ap-templates', 'webpack']);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+    entry: {
+        project: "./src/js/entries/project",
+        // projects: "./src/js/entries/test"
+    },
+    output: {
+        path: path.join(__dirname, "dist/js"),
+        filename: "[name].bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            jquery: "jquery",
+            "window.jQuery": "jquery"
+        })
+        //,
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compress: { warnings: false }
+        // })
+    ]
+};
