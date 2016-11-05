fede83577db290cd4add28c2b28c6a8c10a33ca0
giolf <giovanni.far@gmail.com>
2016-11-05 22:45:48 +0100

    Gulp
    Sass implementation

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,23 @@
-var gulp = require('gulp');
+var gulp    = require('gulp');
 var webpack = require('webpack-stream');
+var sass    = require('gulp-sass');
+var flatten = require('gulp-flatten');
 
 var paths = {
     apTmpl: {
-        src: 'templates/*   ',
+        src: 'templates/*',
         dest: '../../themes/organic_photographer/'
     },
     js: {
-        entries: './src/js/entries/*'
+        src: './src/js/**/*.js'
+    },
+    sass: {
+        src: 'src/sass/**/*.scss',
+        dest: './dist/css/'
     },
     webpack: {
         config: './webpack.config.js',
-        dest: 'dist/js/'
+        dest: './dist/js/'
     }
 };
 
@@ -20,6 +26,12 @@ gulp.task('copy-ap-templates', function() {
     .pipe(gulp.dest(paths.apTmpl.dest));
 });
 
+gulp.task('sass', function() {
+    gulp.src(paths.sass.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(flatten())
+        .pipe(gulp.dest(paths.sass.dest));
+});
 
 gulp.task('webpack', function() {
     return webpack(require(paths.webpack.config))
@@ -28,7 +40,8 @@ gulp.task('webpack', function() {
 
 gulp.task('watch', function() {
     gulp.watch(paths.apTmpl.src, ['copy-ap-templates']);
-    gulp.watch(paths.js.entries, ['webpack']);
+    gulp.watch(paths.js.src, ['webpack']);
+    gulp.watch(paths.sass.src, ['sass']);
 });
 
-gulp.task('default', ['copy-ap-templates', 'webpack']);
+gulp.task('default', ['copy-ap-templates', 'sass', 'webpack']);
