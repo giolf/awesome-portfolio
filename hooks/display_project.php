80550adf4380887e6587302c02f49d9f9f9f6da8
giolf <giovanni.far@gmail.com>
2016-11-12 12:08:03 +0100

    display_project hook check if the main project file is a movie or img

--- hooks/display_project.php
+++ hooks/display_project.php
@@ -5,19 +5,50 @@ function get_project_data() {
 
     global $post;
 
+    // Get the common project's info
     $prj_data = array(
         'title' => $post->post_name,
         'place' => get_post_meta( $post->ID, 'place_project', true ),
-        'featured_prj_image' => wp_get_attachment_image_src(
-            get_post_meta( $post->ID, 'featured_file_project', true ),
-            'featured_prj_image'
-        )[0],
         'date'  => (new DateTime(
             get_post_meta( $post->ID, 'date_project', true )
         ))->format('d-m-Y'),
         'gallery' => array()
     );
 
+    // Check if the main project file is a movie or an image
+    $file_format = wp_get_attachment_metadata(
+        get_post_meta(
+            $post->ID,
+            'featured_file_project',
+            true
+        )
+    )['mime_type'];
+
+    if ($file_format == 'video/quicktime') {
+
+        $prj_data['featured_prj_video'] = wp_get_attachment_url(
+            get_post_meta(
+                $post->ID,
+                'featured_file_project',
+                true
+            )
+        );
+
+        $prj_data['main_file_type'] = 'video';
+
+    }
+    else {
+
+        $prj_data['featured_prj_image'] = wp_get_attachment_image_src(
+            get_post_meta( $post->ID, 'featured_file_project', true ),
+            'featured_prj_image'
+        )[0];
+
+        $prj_data['main_file_type'] = 'img';
+
+    }
+
+    // project's gallery population
     foreach (get_post_meta( $post->ID, 'images_project' ) as $id) {
 
         $thumb = wp_get_attachment_image_src(
@@ -34,7 +65,7 @@ function get_project_data() {
         ]);
 
     }
-    
+
     return $prj_data;
 }
 
