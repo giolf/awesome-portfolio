a1e7bee2fb43a3693e18fa38fb3694bc8299a343
giolf <giovanni.far@gmail.com>
2016-11-13 19:56:32 +0100

    skeleton projects template

---
+++ src/templates/template-projects.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin:   Awesome Portfolio
+ * Template Name: Awesome Portoflio - Projects
+ * Author:   Giovanni Far
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+
+    <div class="row">
+
+        <div class="twelve columns">
+
+            <div id="slideshow" class="slideshow-page">
+                
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<?php get_footer(); ?>
