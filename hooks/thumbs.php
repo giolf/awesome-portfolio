fe78b69e5dbb36981b4702954abc9918312b9971
giolf <giovanni.far@gmail.com>
2016-11-20 19:53:22 +0100

    thumb - featured table project

--- hooks/thumbs.php
+++ hooks/thumbs.php
@@ -6,6 +6,7 @@ function featured_image_size() {
     if ( function_exists( 'add_image_size' ) ) {
         add_image_size( 'featured_prj_image', 900, 400, ['center', 'center'] );
         add_image_size( 'gallery_prj_image', 146, 87, ['center', 'center'] );
+        add_image_size( 'featured_table_project', 444, 296, ['center, center'] );
     }
 }
 
