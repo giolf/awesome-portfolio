53d9f8ef69c1aad6db6f790ce0142e691cf3ece2
giolf <giovanni.far@gmail.com>
2016-12-09 23:18:47 +0100

    global store that keeps global action, mutations, getters and state

--- src/js/entries/projects.js
+++ src/js/entries/projects.js
@@ -5,20 +5,37 @@ require('../../../node_modules/bower_components/uikit/js/core/scrollspy.js');
 // VueJS
 import Vue from 'vue';
 import VueResource from 'vue-resource';
+
+// Vuex
+import Vuex from 'vuex';
+
 // Store
-import store from '../store/projects/store';
-// Getters
+import store from '../store/store';
+
+// Store - Modules
+import projectsModule from '../store/modules/projects/module';
+import moreModule from '../store/modules/more/module';
+
+// Store - Getters
 import { mapGetters } from 'vuex';
+
 // Components
 import Projects from '../components/projects/projects.vue';
 import More from '../components/more/more.vue';
 
 Vue.use(VueResource);
+Vue.use(Vuex);
+
+// Store init
+store.modules = {
+    projectsModule: projectsModule,
+    moreModule: moreModule
+};
 
 window.onload = () => {
     new Vue({
         el: '#app-projects',
-        store,
+        store: new Vuex.Store(store),
         computed: {
             ...mapGetters([
                 'isAppReady'
---
+++ src/js/store/store.js
@@ -0,0 +1,11 @@
+import appState from './state';
+import appGetters from './getters';
+import appActions from './actions';
+import appMutations from './mutations';
+
+export default {
+    state: appState,
+    getters: appGetters,
+    actions: appActions,
+    mutations: appMutations
+};
